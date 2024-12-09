1e0702a589c3876cd68b776a53a3f31b555da213
phillip <kotisphillip@gmail.com>
2024-12-09 10:09:10 -0600

    added .env to client folder

--- TennisPal_Finder/client/api.tsx
+++ TennisPal_Finder/client/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://tennispal-finder.onrender.com";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://tennispal-finder.onrender.com";
 
 
 // API class containing static methods sent to the API
